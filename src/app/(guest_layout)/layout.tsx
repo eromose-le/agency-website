420556e638bf80b88948a623b19e8970c8bac9cc
eromose-le <erons24@yahoo.com>
2024-06-15 14:00:36 +0100

    feat: add metatags

--- src/app/(guest_layout)/layout.tsx
+++ src/app/(guest_layout)/layout.tsx
@@ -1,5 +1,10 @@
 import Footer from "@/common/Footer";
 import Navbar from "@/common/Navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Website",
+};
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
